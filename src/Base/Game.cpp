3161c6f8fb1c7b9afd6691aaec8b27ba3591f98d
Salanem <mail@jan-schmitz.dev>
2024-03-02 17:23:51 +0100

    Fix: swap byte endian

--- src/Base/Game.cpp
+++ src/Base/Game.cpp
@@ -1,13 +1,14 @@
 #include "M5Cardputer.h"
 #include "Game.h"
 #include "Scene.h"
+#include "Scenes/MainScene.h"
 #include "Components/ImageRenderComponent.h"
 #include <M5Unified.h>
 
 void Game::Setup()
 {
     auto cfg = M5.config();
-    M5Cardputer.begin(cfg);
+    M5Cardputer.begin(cfg, true);
 
     int textsize = M5Cardputer.Display.height() / 60;
     if (textsize == 0) 
@@ -15,6 +16,7 @@ void Game::Setup()
         textsize = 1;
     }
     textsize = 1;
+    M5Cardputer.Display.setSwapBytes(true);
     M5Cardputer.Display.setRotation(1);
     M5Cardputer.Display.setTextColor(GREEN);
     M5Cardputer.Display.setTextDatum(middle_center);
@@ -23,6 +25,8 @@ void Game::Setup()
 
     ImageRenderComponent::s_ScreenWidth = M5Cardputer.Display.width();
     ImageRenderComponent::s_ScreenHeight = M5Cardputer.Display.height();
+
+    m_activeScene = new MainScene();
 }
 
 void Game::Run()
@@ -33,7 +37,6 @@ void Game::Run()
     float deltaSeconds = (currentFrame - lastFrame) * 0.001f;
 
     M5Cardputer.update();
-
     if (m_activeScene == nullptr)
     {
         return;

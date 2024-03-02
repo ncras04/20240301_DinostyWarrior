f49b64ec7d2981d8bac612e466edd44b302222c1
Salanem <mail@jan-schmitz.dev>
2024-03-02 01:41:24 +0100

    Feature: add example content

---
+++ src/Game.cpp
@@ -0,0 +1,40 @@
+#include "M5Cardputer.h"
+#include "Game.h"
+#include "GameObject.h"
+#include "SoundObject.h"
+#include <M5Unified.h>
+
+void Game::Setup()
+{
+    auto cfg = M5.config();
+    M5Cardputer.begin(cfg);
+
+    int textsize = M5Cardputer.Display.height() / 60;
+    if (textsize == 0) 
+    {
+        textsize = 1;
+    }
+    textsize = 1;
+    M5Cardputer.Display.setRotation(1);
+    M5Cardputer.Display.setTextColor(GREEN);
+    M5Cardputer.Display.setTextDatum(middle_center);
+    M5Cardputer.Display.setFont(&fonts::Orbitron_Light_32);
+    M5Cardputer.Display.setTextSize(textsize);
+    this->m_objects.push_back(new SoundObject(8000));
+}
+
+void Game::Run()
+{    
+    M5Cardputer.update();
+    for (GameObject* go : this->m_objects)
+    {
+        go->Update(0.2f);
+        delay(100);
+    }
+    // Clear screen
+    M5Cardputer.Display.fillScreen(TFT_BLACK);
+    for (GameObject* go : this->m_objects)
+    {
+        go->Render();
+    }
+}
